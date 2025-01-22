c9f5237223fe6996620c3adcd41c8bd7444abf59
itscrystalline <pvpthadgaming@gmail.com>
2025-01-22 22:34:29 +0700

    rust macros!!! & refactor

--- src/network.rs
+++ src/network.rs
@@ -1,40 +1,59 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
-#[repr(u8)]
-pub enum PacketTypes {
-    Invalid,
-    HelloPacket,
-    PlayerCoordinates
-}
-
-impl Into<u8> for PacketTypes {
-    fn into(self) -> u8 {
-        self as u8
-    }
-}
-impl Into<PacketTypes> for u8 {
-    fn into(self) -> PacketTypes {
-        match self {
-            0 => PacketTypes::HelloPacket,
-            1 => PacketTypes::PlayerCoordinates,
-            _ => PacketTypes::Invalid
-        }
-    }
-}
-#[derive(Serialize, Deserialize, Debug)]
 pub struct Packet {
     pub t: u8,
     #[serde(with = "serde_bytes")]
-    pub data: Vec<u8>
-}
-#[derive(Serialize, Deserialize, Debug)]
-pub struct HelloPacket {
-    pub timestamp: u64
+    pub data: Vec<u8>,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
-pub struct PlayerCoordinates {
-    pub x: i32,
-    pub y: i32
+// https://chatgpt.com/share/67910f66-8c24-8006-bf28-7bc00ff905ed
+macro_rules! define_packets {
+    (
+        $(
+            $name:ident = $value:expr => $struct:ident {
+                $($field_name:ident: $field_type:ty),* $(,)?
+            }
+        ),* $(,)?
+    ) => {
+        #[derive(Serialize, Deserialize, Debug)]
+        #[repr(u8)]
+        pub enum PacketTypes {
+            Invalid = 0,
+            $($name = $value),*
+        }
+
+        impl Into<u8> for PacketTypes {
+            fn into(self) -> u8 {
+                self as u8
+            }
+        }
+
+        impl Into<PacketTypes> for u8 {
+            fn into(self) -> PacketTypes {
+                match self {
+                    $($value => PacketTypes::$name),*,
+                    _ => PacketTypes::Invalid,
+                }
+            }
+        }
+
+        $(
+            #[derive(Serialize, Deserialize, Debug)]
+            pub struct $struct {
+                $($field_name: $field_type),*
+            }
+        )*
+    };
 }
+
+// Use the macro to define packets
+define_packets!(
+    HelloPacket = 1 => HelloPacket {
+        timestamp: u64
+    },
+    PlayerCoordinates = 2 => PlayerCoordinates {
+        x: i32,
+        y: i32
+    }
+);
