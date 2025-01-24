30a7f4df2567d9df7ee5a3726c2052f9797c82f7
itscrystalline <pvpthadgaming@gmail.com>
2025-01-24 13:05:06 +0700

    clienthello impl

--- src/network.rs
+++ src/network.rs
@@ -1,4 +1,7 @@
+use std::net::SocketAddr;
 use serde::{Deserialize, Serialize};
+use rand::prelude::*;
+use serde_pickle::{to_vec, SerOptions};
 use crate::world::{Block, Chunk};
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -6,6 +9,35 @@ pub struct Packet {
     pub t: u8,
     #[serde(with = "serde_bytes")]
     pub data: Vec<u8>,
+}
+
+impl Packet {
+    pub fn encode<T: Serialize>(t: PacketTypes, packet: T) -> serde_pickle::Result<Vec<u8>> {
+        let packet = Packet {
+            t: t.into(),
+            data: to_vec(&packet, SerOptions::new())?,
+        };
+        to_vec(&packet, SerOptions::new())
+    }
+}
+
+pub struct ClientConnection {
+    pub addr: SocketAddr,
+    pub name: String,
+    pub id: u32,
+    pub loaded_chunks: Vec<(u32, u32)>,
+}
+
+impl ClientConnection {
+    pub fn new(addr: SocketAddr, name: String) -> ClientConnection {
+        let mut rng = rand::rng();
+        ClientConnection {
+            addr,
+            name,
+            id: rng.next_u32(),
+            loaded_chunks: Vec::new(),
+        }
+    }
 }
 
 // https://chatgpt.com/share/67910f66-8c24-8006-bf28-7bc00ff905ed
@@ -42,7 +74,7 @@ macro_rules! define_packets {
         $(
             #[derive(Serialize, Deserialize, Debug)]
             pub struct $struct {
-                $($field_name: $field_type),*
+                $(pub $field_name: $field_type),*
             }
         )*
     };
@@ -54,37 +86,37 @@ define_packets!(
         name: String
     },
     ServerSync = 2 => ServerSync {
-        player_id: i32,
-        world_width: i32,
-        world_height: i32,
-        chunk_size: i32
+        player_id: u32,
+        world_width: u32,
+        world_height: u32,
+        chunk_size: u32
     },
     ClientRequestChunk = 3 => ClientRequestChunk {
-        chunk_coords_x: i32,
-        chunk_coords_y: i32,
+        chunk_coords_x: u32,
+        chunk_coords_y: u32,
     },
     ServerChunkResponse = 4 => ServerChunkResponse {
         chunk: Chunk,
     },
     ClientUnloadChunk = 5 => ClientUnloadChunk {
-        chunk_coords_x: i32,
-        chunk_coords_y: i32,
+        chunk_coords_x: u32,
+        chunk_coords_y: u32,
     },
     ServerPlayerJoin = 6 => ServerPlayerJoin {
         player_name: String,
-        player_id: i32
+        player_id: u32
     },
     ServerPlayerEnterLoaded = 7 => ServerPlayerEnterLoaded {
         player_name: String,
-        player_id: i32
+        player_id: u32
     },
     ServerPlayerLeaveLoaded = 8 => ServerPlayerLeaveLoaded {
         player_name: String,
-        player_id: i32
+        player_id: u32
     },
     ServerPlayerLeave = 9 => ServerPlayerLeave {
         player_name: String,
-        player_id: i32
+        player_id: u32
     },
     ClientGoodbye = 10 => ClientGoodbye {},
     ClientPlaceBlock = 11 => ClientPlaceBlock {
@@ -102,7 +134,7 @@ define_packets!(
     },
     ClientPlayerJump = 14 => ClientPlayerJump {},
     ServerPlayerUpdatePos = 15 => ServerPlayerUpdatePos {
-        player_id: i32,
+        player_id: u32,
         pos_x: f32,
         pos_y: f32
     }
