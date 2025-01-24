59a7954ab72cfc58703874f1b7d9f0611dddbb05
itscrystalline <pvpthadgaming@gmail.com>
2025-01-24 10:21:35 +0700

    more utility functions

--- src/main.rs
+++ src/main.rs
@@ -18,9 +18,7 @@ async fn main() -> io::Result<()> {
     env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("debug")).init();
     
     let mut world = World::generate_empty(8, 8, 2).unwrap();
-    debug!("generated world: {:?}", world);
     world.set_block(3, 2, Block::Grass).unwrap();
-    debug!("changed world: {:?}", world);
     
     let socket = UdpSocket::bind("0.0.0.0:8475").await?;
     info!("Listening on {}", socket.local_addr()?);
--- src/world.rs
+++ src/world.rs
@@ -1,3 +1,5 @@
+use log::debug;
+
 #[derive(Debug, Clone)]
 pub enum WorldError {
     MismatchedChunkSize,
@@ -12,7 +14,7 @@ pub struct World {
     pub chunk_size: u32,
     width_chunks: u32,
     height_chunks: u32,
-    pub chunks: Vec<Chunk>
+    pub chunks: Vec<Chunk>,
 }
 
 #[derive(Debug)]
@@ -20,7 +22,7 @@ pub struct Chunk {
     pub size: u32,
     pub chunk_x: u32,
     pub chunk_y: u32,
-    pub blocks: Vec<Block>
+    pub blocks: Vec<Block>,
 }
 
 macro_rules! define_blocks {
@@ -54,11 +56,13 @@ impl World {
         } else {
             let width_chunks = width / chunk_size;
             let height_chunks = height / chunk_size;
-            let chunks = (0..width_chunks * height_chunks).map(|idx| {
-                let chunk_x = idx % width_chunks;
-                let chunk_y = idx / width_chunks;
-                Chunk::empty(chunk_size, chunk_x, chunk_y)
-            }).collect();
+            let chunks = (0..width_chunks * height_chunks)
+                .map(|idx| {
+                    let chunk_x = idx % width_chunks;
+                    let chunk_y = idx / width_chunks;
+                    Chunk::empty(chunk_size, chunk_x, chunk_y)
+                })
+                .collect();
 
             Ok(World {
                 width,
@@ -66,25 +70,46 @@ impl World {
                 chunk_size,
                 width_chunks,
                 height_chunks,
-                chunks
+                chunks,
             })
         }
     }
 
-    pub fn get_chunk_mut(&mut self, chunk_x: u32, chunk_y: u32) -> Result<&mut Chunk, WorldError> {
-        if (chunk_x > self.width / self.chunk_size || chunk_y > self.height / self.chunk_size) {
-            return Err(WorldError::OutOfBounds(chunk_x, chunk_y))
-        }
-        Ok(&mut self.chunks[(chunk_y * self.height_chunks + chunk_x) as usize])
+    fn check_out_of_bounds_chunk(&self, chunk_x: u32, chunk_y: u32) -> bool {
+        chunk_x > self.width / self.chunk_size || chunk_y > self.height / self.chunk_size
+    }
+    fn check_out_of_bounds_block(&self, x: u32, y: u32) -> bool {
+        x >= self.width && y >= self.height
     }
 
+    pub fn get_chunk_mut(&mut self, chunk_x: u32, chunk_y: u32) -> Result<&mut Chunk, WorldError> {
+        if (self.check_out_of_bounds_chunk(chunk_x, chunk_y)) {
+            Err(WorldError::OutOfBounds(chunk_x, chunk_y))
+        } else {
+            Ok(&mut self.chunks[(chunk_y * self.height_chunks + chunk_x) as usize])
+        }
+    }
+
+    pub fn get_chunk(&self, chunk_x: u32, chunk_y: u32) -> Result<&Chunk, WorldError> {
+        if (self.check_out_of_bounds_chunk(chunk_x, chunk_y)) {
+            Err(WorldError::OutOfBounds(chunk_x, chunk_y))
+        } else {
+            Ok(&self.chunks[(chunk_y * self.height_chunks + chunk_x) as usize])
+        }
+    }
+    
     pub fn set_block(&mut self, pos_x: u32, pos_y: u32, block: Block) -> Result<(), WorldError> {
+        if (self.check_out_of_bounds_block(pos_x, pos_y)) {
+            return Err(WorldError::OutOfBounds(pos_x, pos_y));
+        }
+        
         let chunk_x = pos_x / self.chunk_size;
         let chunk_y = pos_y / self.chunk_size;
         let pos_inside_chunk_x = pos_x - chunk_x * self.chunk_size;
         let pos_inside_chunk_y = pos_y - chunk_y * self.chunk_size;
 
         let chunk = self.get_chunk_mut(chunk_x, chunk_y)?;
+        debug!("Found chunk: {:?}", chunk);
         chunk.set_block(pos_inside_chunk_x, pos_inside_chunk_y, block);
         Ok(())
     }
@@ -93,13 +118,20 @@ impl World {
 impl Chunk {
     fn empty(size: u32, chunk_x: u32, chunk_y: u32) -> Chunk {
         Chunk {
-            size, chunk_x, chunk_y,
+            size,
+            chunk_x,
+            chunk_y,
             blocks: (0..size.pow(2)).map(|_| Block::Air).collect(),
         }
     }
 
     fn set_block(&mut self, chunk_pos_x: u32, chunk_pos_y: u32, block: Block) -> &mut Self {
-        self.blocks[(chunk_pos_y * self.size + chunk_pos_x) as usize] = block;
+        let idx = (chunk_pos_y * self.size + chunk_pos_x) as usize;
+        self.blocks[idx] = block;
+        debug!(
+            "[Chunk at ({}, {})] Set block index {} to {:?}",
+            self.chunk_x, self.chunk_y, idx, block
+        );
         self
     }
 }
