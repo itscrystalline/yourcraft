a929ae2c446bf51e0ff77b1b340e434e39b1a0cc
itscrystalline <pvpthadgaming@gmail.com>
2025-01-25 17:14:58 +0700

    make main tick loop run at a fixed rate

--- rs/main.rs
+++ rs/main.rs
@@ -2,6 +2,7 @@ use tokio::net::UdpSocket;
 use std::io;
 use log::info;
 use crate::world::{Block, World};
+use tokio::time::{self, Duration};
 
 mod network;
 mod world;
@@ -10,15 +11,19 @@ mod player;
 #[tokio::main]
 async fn main() -> io::Result<()> {
     env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("debug")).init();
+    
+    let mut clock = time::interval(Duration::from_millis(20));
 
     let mut world = World::generate_empty(8, 8, 2).unwrap();
     world.set_block(3, 2, Block::Grass).unwrap();
     
     let socket = UdpSocket::bind("0.0.0.0:8475").await?;
-    info!("Listening on {}", socket.local_addr()?);
     let mut buf = [0u8; 1024];
+    info!("Listening on {}", socket.local_addr()?);
     
     loop {
+        clock.tick().await;
+        
         network::network_handler(&socket, &mut buf, &mut world).await?
     }
 }
