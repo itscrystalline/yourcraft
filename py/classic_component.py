8f1e89ed3eff601b632964dc71909f625b555695
FujiForm2023 <jufi.easik@gmail.com>
2025-01-25 18:06:06 +0900

    Add block placement (Beta)

--- py/classic_component.py
+++ py/classic_component.py
@@ -1,43 +1,91 @@
 # Import needs
 from entities import Component
 import dataclasses
+from typing import Union, Any
 
 
 @dataclasses.dataclass
 class Position2D(Component):
-    x : float = 0
-    y : float = 0
+    x: float = 0
+    y: float = 0
+
+    def __add__(self, other: Union["Position2D", tuple, list]):
+        if isinstance(other, Position2D):
+            return Position2D(x=self.x + other.x, y=self.y + other.y)
+        else:
+            return Position2D(x=self.x + other[0], y=self.y + other[1])
+
+    def __sub__(self, other: Union["Position2D", tuple, list]):
+        if isinstance(other, Position2D):
+            return Position2D(x=self.x - other.x, y=self.y - other.y)
+        else:
+            return Position2D(x=self.x - other[0], y=self.y - other[1])
+
+    def __mul__(self, other: float | int | complex):
+        return Position2D(x=self.x * other, y=self.y * other)
+
+    def __str__(self):
+        return f"Position2D x={self.x}, y={self.y}"
 
 
 @dataclasses.dataclass
 class Rotation2D(Component):
-    x : float = 0
+    _x: float = 0
+
+    def __init__(self, x: float | int = 0) -> None:
+        self._x = x
+        super().__init__()
+
+    @property
+    def x(self) -> float:
+        return self._x
+
+    @x.setter
+    def x(self, value: float) -> None:
+        self._x = value % 360
+        if self._x < 0:
+            self._x += 360
+
+    def __add__(self, other: Union["Rotation2D", float, int]):
+        if isinstance(other, Rotation2D):
+            return Rotation2D(x=self._x + other._x)
+        else:
+            return Rotation2D(x=self._x + other)
+
+    def __sub__(self, other: Union["Rotation2D", float, int]):
+        if isinstance(other, Rotation2D):
+            return Rotation2D(x=self._x - other._x)
+        else:
+            return Rotation2D(x=self._x - other)
+
+    def __mul__(self, other: float | int):
+        return Rotation2D(x=self._x * other)
 
 
 @dataclasses.dataclass
 class Transform2D(Component):
-    position : Position2D = dataclasses.field(default_factory=Position2D)
-    rotation : Rotation2D = dataclasses.field(default_factory=Rotation2D)
+    position: Position2D = dataclasses.field(default_factory=Position2D)
+    rotation: Rotation2D = dataclasses.field(default_factory=Rotation2D)
 
 
 @dataclasses.dataclass
 class Health(Component):
-    current : int = 100
-    maximum : int = 100
+    current: int = 100
+    maximum: int = 100
 
 
 @dataclasses.dataclass
 class Cooldown(Component):
-    current : float = 0
-    maximum : float = 1
+    current: float = 0
+    maximum: float = 1
 
 
 @dataclasses.dataclass
 class ImageSprite(Component):
-    image_path : str = None
-    layer : int = 0
+    image_path: str = None
+    layer: int = 0
 
 
 @dataclasses.dataclass
 class Inventory(Component):
-    items : dict[str, int] = dataclasses.field(default_factory=dict)
+    items: dict[str, int] = dataclasses.field(default_factory=dict)
--- py/classic_entity.py
+++ py/classic_entity.py
@@ -4,8 +4,8 @@ import classic_handler
 import dataclasses
 
 class Player(Entity):
-    keys = ["w","a","s","d"]
+    keys = ["w","a","s","d","e","q"]
     def __post_init__(self):
         super().__post_init__()
 
-        self.addComponent("transfrom2D",classic_component.Transform2D())
+        self.addComponent("transform2D",classic_component.Transform2D())
