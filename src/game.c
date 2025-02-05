d4476bd50c46f5e6cf4e549a52d560780914d73f
napookooo <napookooo4789@gmail.com>
2025-02-05 09:59:45 +0700

    add attack

--- src/game.c
+++ src/game.c
@@ -12,8 +12,12 @@ void loadGame(GameState *mState){
   // Initilize player
   mState->man.x = 960;
   mState->man.y = 540;
+  mState->man.lookDirection = 1;
   mState->man.size_x = 100;
   mState->man.size_y = 200;
+  mState->man.attack = 0;
+  mState->man.att_sx = 50;
+  mState->man.att_sy = 50;
 
   mState->keyState.space = 0;
   mState->keyState.o = 0;
@@ -41,8 +45,7 @@ void loadGame(GameState *mState){
 void events(GameState *mState){
 
   for (int i=0; i<mState->platform_used; i++){
-    int collision = playerCollision(&mState->man, mState->plat[i].cx, mState->plat[i].cy);
-    printf("%d\n", collision);
+    playerCollision(&mState->man, mState->plat[i].cx, mState->plat[i].cy);
   }
 
   mState->man.cx[0] = mState->man.x;
@@ -50,7 +53,66 @@ void events(GameState *mState){
   mState->man.cx[1] = mState->man.x+mState->man.size_x;
   mState->man.cy[1] = mState->man.y+mState->man.size_y;
 
+  mState->man.x += mState->man.vx;
+  mState->man.y += mState->man.vy;
 
+  playerLimits(&mState->man);
+  mState->man.vy += 4;
+
+  if (!mState->man.lookDirection){
+    mState->man.att_x = mState->man.x - 50;
+  }
+  else {
+    mState->man.att_x = mState->man.x + 100;
+  }
+  // if (mState->man.attack) {
+  //   mState->man.attack = 0;
+  // }
+
+}
+
+int playerLimits(Man *man){
+
+  if (man->vx > 20) {
+    man->vx = 20;
+  }
+  else if (man->vx < -20) {
+    man->vx = -20;
+  }
+  if (man->vy > 20) {
+    man->vy = 20;
+  }
+  else if (man->vy < -20) {
+    man->vy = -20;
+  }
+
+  if (man->vx < 20 && man->vx > 5) {
+    man->vx -= 5;
+  }
+  else if (man->vx > -20 && man->vx < -5) {
+    man->vx += 5;
+  }
+  if (man->vy < 20 && man->vy > -5) {
+    man->vy -= 5;
+  }
+  else if (man->vy > -20 && man->vy < -5) {
+    man->vy += 5;
+  }
+
+  if (man->vx <= 5 && man->vx >= 0) {
+    man->vx = 0;
+  }
+  else if (man->vx >= -5 && man->vx <= 0) {
+    man->vx = 0;
+  }
+  if (man->vy <= 5 && man->vy >= 0) {
+    man->vy = 0;
+  }
+  else if (man->vy >= -5 && man->vy <= 0) {
+    man->vy = 0;
+  }
+
+  return 0;
 
 }
 
@@ -72,20 +134,20 @@ int processInputs(SDL_Window *window, GameState *mState){
         switch (event.key.keysym.sym) {
           case SDLK_ESCAPE:
             done = 1;
-          case SDLK_p:
-            if (!mState->keyState.p){
-              mState->keyState.p = 1;
-            }
-            else {
-              mState->keyState.p = 0;
-            }
-          case SDLK_o:
-            if (!mState->keyState.o){
-              mState->keyState.o = 1;
-            }
-            else {
-              mState->keyState.o = 0;
-            }
+          // case SDLK_p:
+          //   if (!mState->keyState.p){
+          //     mState->keyState.p = 1;
+          //   }
+          //   else {
+          //     mState->keyState.p = 0;
+          //   }
+          // case SDLK_o:
+          //   if (!mState->keyState.o){
+          //     mState->keyState.o = 1;
+          //   }
+          //   else {
+          //     mState->keyState.o = 0;
+          //   }
           case SDLK_t:
             printf("vx: %.2f\n", mState->man.vx);
             printf("vy: %.2f\n", mState->man.vy);
@@ -101,20 +163,21 @@ int processInputs(SDL_Window *window, GameState *mState){
 
   const Uint8 *state = SDL_GetKeyboardState(NULL);
   if(state[SDL_SCANCODE_A]){
-    // playerMoveLeft(&mState->man);
-    mState->man.x -= 10;
+    mState->man.vx -= 10;
+    mState->man.lookDirection = 0;
   }
   if(state[SDL_SCANCODE_D]){
-    // playerMoveRight(&mState->man);
-    mState->man.x += 10;
+    mState->man.vx += 10;
+    mState->man.lookDirection = 1;
   }
-  if(state[SDL_SCANCODE_W]){
-    // playerMoveLeft(&mState->man);
-    mState->man.y -= 10;
+  if(state[SDL_SCANCODE_SPACE]){
+    mState->man.vy -= 10;
   }
-  if(state[SDL_SCANCODE_S]){
-    // playerMoveRight(&mState->man);
-    mState->man.y += 10;
+  if(state[SDL_SCANCODE_L]){
+    mState->man.attack = 1;
+  }
+  else {
+    mState->man.attack = 0;
   }
   if(state[SDL_SCANCODE_R]){
     mState->man.x = 500;   mState->man.y = 500;
--- src/game.h
+++ src/game.h
@@ -6,5 +6,6 @@
 void loadGame(GameState *mState);
 void events(GameState *mState);
 int processInputs(SDL_Window *window, GameState *mState);
+int playerLimits(Man *man);
 
 #endif
