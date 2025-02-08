81b7e6c43ec1bc07eab8cba00ffaf32ceddb8b01
itscrystalline <pvpthadgaming@gmail.com>
2025-02-08 12:48:41 +0700

    devenv: add static building support

--- devenv.nix
+++ devenv.nix
@@ -1,6 +1,6 @@
 { pkgs, lib, config, inputs, ... }:
 
-{
+let cc_aarch64 = pkgs.pkgsCross.aarch64-multiplatform.stdenv.cc; in {
   cachix.enable = false;
 
   env.LD_LIBRARY_PATH = "${pkgs.libglvnd}/lib";
@@ -18,12 +18,14 @@
     }))
   ]) ++ pkgs.lib.optional (pkgs.system == "x86_64-linux") pkgs.gcc_multi;
 
+  env.CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER = "${cc_aarch64}/bin/${cc_aarch64.targetPrefix}cc";
+  env.CARGO_TARGET_AARCH64_UNKNOWN_LINUX_MUSL_LINKER = "${cc_aarch64}/bin/${cc_aarch64.targetPrefix}cc";
   # https://devenv.sh/languages/
   languages.rust = {
     enable = true;
     channel = "stable";
     components = [ "rustc" "cargo" "clippy" "rustfmt" "rust-analyzer" "rust-src" ];
-    targets = [ "x86_64-unknown-linux-gnu" "aarch64-unknown-linux-gnu" "i686-unknown-linux-gnu" ];
+    targets = [ "x86_64-unknown-linux-gnu" "aarch64-unknown-linux-gnu" "i686-unknown-linux-gnu" "x86_64-unknown-linux-musl" "aarch64-unknown-linux-musl" ];
   };
   languages.python = {
     enable = true;
